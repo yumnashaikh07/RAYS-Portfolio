cbcde2aa04856e00ca22d683f28ad50b419f08dc
yumnashaikh07 <164198271+yumnashaikh07@users.noreply.github.com>
2024-12-04 02:28:04 +0500

    changed text for responsiveness

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -2,7 +2,7 @@ import { FaEnvelope, FaPhone, FaLinkedin,  FaGithub} from "react-icons/fa";
 
 const ContactIcons = () => {
   return (
-    <main><h1 className='text-3xl md:ml-16 mx-auto font-bold '>GET TO KNOW MORE</h1>
+    <main><h1 className='text-3xl md:ml-14 mx-auto font-bold '>GET TO KNOW MORE</h1>
     <section className=" mt-3 p-5  text-black flex">
     
       <a
@@ -23,7 +23,7 @@ const ContactIcons = () => {
         href="tel:+923132480307"
         aria-label="Phone"
       >
-        <FaPhone size={31} className="md:ml-[77px] ml-11 text-blue-700 hover:text-white" />
+        <FaPhone size={31} className="md:ml-14 ml-10 text-blue-700 hover:text-white" />
        
       </a>
       <a
@@ -33,15 +33,15 @@ const ContactIcons = () => {
         aria-label="LinkedIn"
         className=" text-[#463976] hover:text-white"
       >
-        <FaGithub size={37}  className="md:ml-12 ml-9" />
+        <FaGithub size={37}  className="md:ml-11 ml-8" />
       </a>
 
       </section>
       <section className="font-semibold  text-black flex ">
       <div className="md:ml-14 ml-3  hover:text-[#1f4676]">Email</div>
       <div className="md:ml-11 ml-5 hover:text-[#1f4676]">LinkedIn</div>
-      <div className="md:ml-12 ml-5 hover:text-[#1f4676]">Phone</div>
-      <div className="md:ml-9 ml-5 hover:text-[#1f4676] ">Github</div>
+      <div className="md:ml-7 ml-5 hover:text-[#1f4676]">Phone</div>
+      <div className="md:ml-7 ml-5 hover:text-[#1f4676] ">Github</div>
       </section>
     </main>
   );
